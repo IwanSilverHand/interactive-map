ac5d381369838c34f46d3452cfa47b7e47a38fa1
IwanSilverHand <ivan_lopez_palomo@outlook.com>
2021-09-18 13:28:24 -0500

    Funcionamiento completo y botones automatizados

--- public/js/air.js
+++ public/js/air.js
@@ -1,16 +1,21 @@
+// Script que consigue info de la API de Airtable
 console.log('Conexion al JS de Airtable');
 const API_KEY = 'key0xswwEgtvrbQ2J';
 const Airtable_URL = `https://api.airtable.com/v0/appwBNFT7XIhOATPt/Locations?api_key=${API_KEY}`;
 
 async function getAirData(){
-    let num = Math.floor(Math.random()*4);
     const response = await fetch(Airtable_URL);
     const data =  await response.json();
-    let img = data.records[num].fields.Thumbnail[0].url;
-    document.getElementById("location_title").textContent = data.records[num].fields.Name;
-    document.getElementById('location_category').textContent = data.records[num].fields.Categoria;
-    document.getElementById('location_description').textContent = data.records[num].fields.City;
-    document.getElementById('location_image').style.backgroundImage = "url(" + img + ")";
+    let img = data.records[id].fields.Thumbnail[0].url;
+    document.getElementById("location_title").textContent = data.records[id].fields.Name;
+    document.getElementById('location_category').textContent = data.records[id].fields.Categoria;
+    document.getElementById('location_description').textContent = data.records[id].fields.Address;
+    document.getElementById('location_image').style.backgroundImage = `url("${img}")`;
+    latitud = data.records[id].fields.Latitud;
+    longitud = data.records[id].fields.Longitud;
+    mymap.setView([latitud, longitud], 15);
+    //console.log('Hola Mundo');
+    /*for (const record of data.records) {
+        console.log(record);
+      }*/
 }
-
-getAirData();
--- public/js/index.js
+++ public/js/index.js
@@ -1,40 +1,66 @@
-// Ligar al API
-const API_URL = "https://jsonplaceholder.typicode.com/users/1";
-const request = new XMLHttpRequest();
-let num;
-
-// Ctlr + K + C para comentar lineas de codigo
-// Ligar objetos del DOM a variables"
-// let picture = document.querySelector("#location_image");
-// let title = document.querySelector("#location_title");
-// let category = document.querySelector("#location_category");
-// let description = document.querySelector("#location_description");
-
-//Aquí ligo el boton de "Prueba"
-let toogle = document.querySelector("#toogle");
-toogle.addEventListener("click",openModal,false);
-
-//Aquí ligo el "Modal"
-let modal = document.querySelector("#modal");
+// Definir variables y ligar objetos del DOM a variables"
+const modal = document.querySelector("#modal");
+const navbar = document.querySelector(".navbar");
 let modalValue = false;
+let id;
+let latitud;
+let longitud;
+let response;
+let data;
+let img;
+
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });
+let mymap = L.map('mapid');
+
+//Aquí inicializo el mapa consigo la ubicación.
+navigator.geolocation.getCurrentPosition(function(position){
+  latitud = position.coords.latitude;
+  longitud = position.coords.longitude;
+
+  //Esto ajusta el mapa y a la ubicación del dispositivo.
+  mymap.setView([latitud, longitud], 13);
+  tiles.addTo(mymap);
+  //console.log(latitud,longitud);
+});
+
+//Esta función revisa que boton hizo click
+navbar.addEventListener("click", e => {
+  const target = e.target.closest(".w-button"); // see if the click landed inside a button
+  if (!target) return; // bail if it wasn't inside a button
+  // do stuff with target here (target is now the button that was clicked on)
+  id = target.dataset.index;
+  openModal()
+})
+
+// Funcion que trae los datos de Airtable.
+async function getAirData(){
+  const API_KEY = 'key0xswwEgtvrbQ2J';
+  const Airtable_URL = `https://api.airtable.com/v0/appwBNFT7XIhOATPt/Locations?api_key=${API_KEY}`;
+  response = await fetch(Airtable_URL);
+  data =  await response.json();
+  //Loop que genera los botones.
+     for (let i = 0; i < data.records.length; i++) {
+      let newButton = document.createElement('div');
+      newButton.setAttribute('class','w-button');
+      newButton.innerText = data.records[i].fields.Location;
+      newButton.setAttribute('data-index', i);
+      navbar.appendChild(newButton);
+      }
+}
 
 // Función que abre el Modal
 function openModal()
 {
-    getAirData()
-    /*
-    request.addEventListener("load",requestData);
-    request.open('GET', `${API_URL}/users`);
-    request.send();
-    */
-    
-    // Codigo que muestra el Modal
     switch(modalValue)
     {
         case false:
+            print()
             modal.style.display = "flex";
             modalValue = true;
             break;
+
         case true:
             modal.style.display = "none";
             modalValue = false;
@@ -42,15 +68,18 @@ function openModal()
     }
 }
 
-// Funcion que hace el Request
-/*
-function requestData(){
-    if (this.readyState === 4 && this.status === 200){
-        const apiData = JSON.parse(this.response);
-        num = Math.floor(Math.random() * 10);
-        location_title.innerHTML = apiData[num].name;
-        location_category.innerHTML = apiData[num].address.zipcode;
-        location_description.innerHTML = apiData[num].company.catchPhrase;
-        };
+// Esta funcion imprime los datos del modal
+function print(){
+  // Aqui se imprimen los datos de airtable.
+  document.getElementById("location_title").textContent = data.records[id].fields.Location;
+  document.getElementById('location_category').textContent = data.records[id].fields.Category;
+  document.getElementById('location_description').textContent = data.records[id].fields.Address;
+  img = data.records[id].fields.Picture[0].url;
+  document.getElementById('location_image').style.backgroundImage = `url("${img}")`;
+  latitud = data.records[id].fields.Latitude;
+  longitud = data.records[id].fields.Longitude;
+  mymap.setView([latitud, longitud], 15);
 }
-*/
+
+window.onload = getAirData()
+// Ctlr + K + C para comentar lineas de codigo
